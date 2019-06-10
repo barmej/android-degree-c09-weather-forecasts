fb8a8bb8c4a07e4faf9f37ce0ee9737739cb70f1
oza <omar.albelbaisy@gmail.com>
2019-06-10 18:03:40 +0300

    Add weather info widget to the app

--- app/src/main/java/com/barmej/weatherforecasts/widget/WeatherWidgetProvider.java
+++ app/src/main/java/com/barmej/weatherforecasts/widget/WeatherWidgetProvider.java
@@ -1,29 +1,112 @@
 package com.barmej.weatherforecasts.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.widget.RemoteViews;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+
 import com.barmej.weatherforecasts.R;
+import com.barmej.weatherforecasts.data.WeatherDataRepository;
+import com.barmej.weatherforecasts.data.entity.WeatherInfo;
+import com.barmej.weatherforecasts.data.sync.SyncUtils;
+import com.barmej.weatherforecasts.ui.activities.MainActivity;
+import com.barmej.weatherforecasts.utils.CustomDateUtils;
+import com.barmej.weatherforecasts.utils.WeatherUtils;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class WeatherWidgetProvider extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    static void updateAppWidget(final Context context, final AppWidgetManager appWidgetManager, final int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Get current widget width and height
+        Bundle options = appWidgetManager.getAppWidgetOptions(appWidgetId);
+        int widgetHeight = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT);
+        int widgetWidth = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
 
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.weather_widget_provider);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        final RemoteViews views;
 
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        // Determine the widget layout file based on the current widget width and height
+        if (widgetHeight >= 120 && widgetWidth >= 180) {
+            views = new RemoteViews(context.getPackageName(), R.layout.weather_widget_large);
+        } else if (widgetWidth > 120) {
+            views = new RemoteViews(context.getPackageName(), R.layout.weather_widget_medium);
+        } else {
+            views = new RemoteViews(context.getPackageName(), R.layout.weather_widget_small);
+        }
+
+
+        // Get the data from the data base
+        WeatherDataRepository weatherDataRepository = WeatherDataRepository.getInstance(context);
+        LiveData<WeatherInfo> weatherInfo = weatherDataRepository.getWeatherInfo();
+
+        weatherInfo.observeForever(new Observer<WeatherInfo>() {
+            @Override
+            public void onChanged(@Nullable WeatherInfo weatherInfo) {
+
+                // Make sure we got the data successfully
+                if (weatherInfo != null) {
+
+                    // Get human readable string from timestamp using getFriendlyDateString utility method
+                    String dateString = CustomDateUtils.getFriendlyDateString(context, weatherInfo.getDt(), false);
+
+                    // Display friendly date string
+                    views.setTextViewText(R.id.appwidget_text_date, dateString);
+
+                    // Get formatted temperature with degree symbol
+                    String temperature = context.getString(R.string.format_temperature, weatherInfo.getMain().getTemp());
+
+                    // Display current temperature
+                    views.setTextViewText(R.id.appwidget_text_temperature, temperature);
+
+                    // Use getWeatherIcon utility method to get weather icon resource ID
+                    int weatherImageId = WeatherUtils.getWeatherIcon(weatherInfo.getWeather().get(0).getIcon());
+
+                    // Display the icon
+                    views.setImageViewResource(R.id.appwidget_image_weather_icon, weatherImageId);
+
+                    // Display the current city name
+                    views.setTextViewText(R.id.appwidget_text_city, weatherInfo.getName());
+
+                } else {
+
+                    // If database is empty, try to get data from the internet and update the db
+                    SyncUtils.startSync(context);
+
+                }
+
+                // Instruct the widget manager to update the widget
+                appWidgetManager.updateAppWidget(appWidgetId, views);
+            }
+        });
+
+        // Set click Listener to open main activity when the user click on the main widget view
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.appwidget_container, pendingIntent);
+
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        updateAppWidget(context, appWidgetManager, appWidgetId);
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
